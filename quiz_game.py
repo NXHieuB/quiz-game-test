efb359e23bde9ac398310125c1dfcfa4160e528e
Nguyen Xuan Hieu <104493696+sportsengineeringvn@users.noreply.github.com>
2023-05-09 09:32:13 +0700

    Update quiz_game.py

--- quiz_game.py
+++ quiz_game.py
@@ -1,4 +1,5 @@
 import csv
+import time
 
 def load_questions(file_path):
     questions = []
@@ -13,20 +14,25 @@ def load_questions(file_path):
             questions.append(question)
     return questions
 
-def ask_question(question):
+def ask_question(question, time_limit):
     print(question['text'])
     for i, option in enumerate(question['options']):
         print(f"{i+1}. {option}")
-    answer = input("Your answer (1-4): ")
+    start_time = time.time()
+    answer = input(f"Your answer (1-4). You have {time_limit} seconds: ")
+    elapsed_time = time.time() - start_time
     print()
+    if elapsed_time > time_limit:
+        print(f"Time's up! You took too long to answer the question.")
+        return False
     return answer == question['answer']
 
-def run_quiz():
+def run_quiz(time_limit):
     file_path = 'quiz_questions.csv'
     questions = load_questions(file_path)
     score = 0
     for question in questions:
-        if ask_question(question):
+        if ask_question(question, time_limit):
             score += 1
     print(f"Your final score is {score}/{len(questions)}.")
 
@@ -39,8 +45,9 @@ def run_quiz():
     with open('scores.txt', 'r') as file:
         scores = [int(line.strip()) for line in file]
         scores.sort(reverse=True)
-        for i, score in enumerate(scores):
+        for i, score in enumerate(scores[:10]):
             print(f"{i+1}. {score}")
 
 if __name__ == "__main__":
-    run_quiz()
+    time_limit = 10
+    run_quiz(time_limit)
